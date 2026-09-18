41edab50c47b3034ff8549407f2d2a459c88f783
benjamin1734 <muzafferbatingurnes@gmail.com>
2026-09-18 21:02:12 +0300

    feat(11.04): add bug reporter domain services

---
+++ core/Bug/Report/BugReportNotifier.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Forwext\Core\Bug\Report;
+
+interface BugReportNotifier
+{
+    public function staffResponse(BugReport $report, BugReportHistoryEntry $entry): void;
+
+    public function statusChanged(BugReport $report): void;
+
+    public function reporterInfoAdded(BugReport $report, BugReportHistoryEntry $entry): void;
+}
