87106680539a57ad78a5a6eddbb59e1eb4a56790
benjamin1734 <muzafferbatingurnes@gmail.com>
2026-09-18 22:24:48 +0300

    feat(12.02): register AI moderation override migration

--- core/Install/CoreMigrationRegistry.php
+++ core/Install/CoreMigrationRegistry.php
@@ -7,6 +7,7 @@ namespace Forwext\Core\Install;
 use Forwext\Core\Migration\Migration;
 use Forwext\Database\Migrations\Core\AddDiscoveryQueryIndexes;
 use Forwext\Database\Migrations\Core\CreateAbusePreventionTables;
+use Forwext\Database\Migrations\Core\CreateAiModerationOverrides;
 use Forwext\Database\Migrations\Core\CreateAttachmentPipelineTables;
 use Forwext\Database\Migrations\Core\CreateAuthenticationRuntimeTables;
 use Forwext\Database\Migrations\Core\CreateContentModerationTables;
@@ -110,6 +111,7 @@ final class CoreMigrationRegistry
             new CreateBugReportFormIntake(),
             new CreateBugReportConversation(),
             new CreateBugStaffWorkflow(),
+            new CreateAiModerationOverrides(),
         ];
     }
 }
