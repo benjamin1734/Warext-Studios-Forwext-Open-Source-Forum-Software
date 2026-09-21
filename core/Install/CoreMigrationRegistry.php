a427fd349b759c17da7680da261f8cc4dfcc793b
benjamin1734 <muzafferbatingurnes@gmail.com>
2026-09-21 11:53:33 +0300

    fix(14.03): import external-sale migration

--- core/Install/CoreMigrationRegistry.php
+++ core/Install/CoreMigrationRegistry.php
@@ -38,6 +38,7 @@ use Forwext\Database\Migrations\Core\CreatePromotionSystem;
 use Forwext\Database\Migrations\Core\AddPromotionRevocationPolicy;
 use Forwext\Database\Migrations\Core\CreateMarketplaceDomain;
 use Forwext\Database\Migrations\Core\CreateMarketplaceDiscoveryUx;
+use Forwext\Database\Migrations\Core\CreateMarketplaceExternalSale;
 use Forwext\Database\Migrations\Core\CreateSupportReportingAudit;
 use Forwext\Database\Migrations\Core\CreateBugReportWorkflow;
 use Forwext\Database\Migrations\Core\CreateBugDiagnosticContext;
