ee42f026034c8c99ece624ab2dcd7ed922e762e9
benjamin1734 <muzafferbatingurnes@gmail.com>
2026-09-21 18:36:22 +0300

    feat(14.07): register subscription migration

--- core/Install/CoreMigrationRegistry.php
+++ core/Install/CoreMigrationRegistry.php
@@ -44,6 +44,7 @@ use Forwext\Database\Migrations\Core\CreateMarketplaceDigitalDelivery;
 use Forwext\Database\Migrations\Core\CreatePaymentAbstraction;
 use Forwext\Database\Migrations\Core\ScopePaymentRefundReference;
 use Forwext\Database\Migrations\Core\CreateSupportReportingAudit;
+use Forwext\Database\Migrations\Core\CreateSubscriptionUpgradeSystem;
 use Forwext\Database\Migrations\Core\CreateBugReportWorkflow;
 use Forwext\Database\Migrations\Core\CreateBugDiagnosticContext;
 use Forwext\Database\Migrations\Core\CreateBugReportFormIntake;
@@ -162,6 +163,7 @@ final class CoreMigrationRegistry
             new CreatePaymentAbstraction(),
             new ScopePaymentRefundReference(),
             new CreateMarketplaceDigitalDelivery(),
+            new CreateSubscriptionUpgradeSystem(),
         ];
     }
 }
