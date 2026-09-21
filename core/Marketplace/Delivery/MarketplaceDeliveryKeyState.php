cf4043fce255c4d1692334c9ec0c7842659b5411
benjamin1734 <muzafferbatingurnes@gmail.com>
2026-09-21 13:29:24 +0300

    feat(14.06): reserve delivery keys during checkout

--- core/Marketplace/Delivery/MarketplaceDeliveryKeyState.php
+++ core/Marketplace/Delivery/MarketplaceDeliveryKeyState.php
@@ -7,6 +7,7 @@ namespace Forwext\Core\Marketplace\Delivery;
 enum MarketplaceDeliveryKeyState:string
 {
     case Available='available';
+    case Reserved='reserved';
     case Assigned='assigned';
     case Revoked='revoked';
 }
