854d8543b22e6de8f8c8ab39c71c39269e22f8d3
benjamin1734 <muzafferbatingurnes@gmail.com>
2026-09-23 18:56:16 +0300

    test(15.02): require forum analytics migration in installer

--- tests/Unit/Core/Migration/CoreMigrationRegistryTest.php
+++ tests/Unit/Core/Migration/CoreMigrationRegistryTest.php
@@ -40,6 +40,7 @@ use Forwext\Database\Migrations\Core\CreateSupportReportingAudit;
 use Forwext\Database\Migrations\Core\CreateSubscriptionUpgradeSystem;
 use Forwext\Database\Migrations\Core\CreateAdvertisingNoticeSystem;
 use Forwext\Database\Migrations\Core\CreateAnalyticsEventModel;
+use Forwext\Database\Migrations\Core\CreateForumAnalyticsDashboard;
 use Forwext\Database\Migrations\Core\CreateBugReportWorkflow;
 use Forwext\Database\Migrations\Core\CreateBugDiagnosticContext;
 use Forwext\Database\Migrations\Core\CreateBugReportFormIntake;
@@ -145,5 +146,6 @@ final class CoreMigrationRegistryTest extends TestCase
         self::assertContains(CreateSubscriptionUpgradeSystem::class, $classes);
         self::assertContains(CreateAdvertisingNoticeSystem::class, $classes);
         self::assertContains(CreateAnalyticsEventModel::class, $classes);
+        self::assertContains(CreateForumAnalyticsDashboard::class, $classes);
     }
 }
